a1ddd8c5ec50cf3901c9f4d29efe9cc8370a580a
Hee-jin506 <gmlwls35220@gmail.com>
2020-10-13 19:45:47 +0900

    Do it! ~141p

--- src/main/java/com/heejin/programmers/No43238.java
+++ src/main/java/com/heejin/programmers/No43238.java
@@ -1,37 +1,37 @@
 package com.heejin.programmers;
 
 public class No43238 {
-    static long solution(int n, int[] times) {
-        Customer[] customers = new Customer[n];
-        for (int i = 0; i < times.length; i++) {
-            customers[i] = new Customer(times[i], times[i]);
+  static long solution(int n, int[] times) {
+    Customer[] customers = new Customer[n];
+    for (int i = 0; i < times.length; i++) {
+      customers[i] = new Customer(times[i], times[i]);
+    }
+
+    for  (int i = times.length; i < n; i++) {
+      long min = customers[i - 1].time + customers[i - 1].num;
+      long num = 0;
+      for (int j = i - 1; j >= i - times.length; j--) {
+        if (min > customers[j].time + customers[j].num) {
+          min = customers[j].time + customers[j].num;
+          num = customers[j].num;
         }
-        
-        for  (int i = times.length; i < n; i++) {
-            long min = customers[i - 1].time + customers[i - 1].num;
-            long num = 0;
-            for (int j = i - 1; j >= i - times.length; j--) {
-                if (min > customers[j].time + customers[j].num) {
-                    min = customers[j].time + customers[j].num;
-                    num = customers[j].num;
-                }
-                customers[i] = new Customer(min, num);
-            }
-        }
-        return customers[n - 1].time;
+        customers[i] = new Customer(min, num);
+      }
     }
-    
-    static class Customer {
-        long time = 0;
-        long num = 0;
-        
-        public Customer(long time, long num) {
-            this.time = time;
-            this.num = num;
-        } 
-    }
-    
-    public static void main(String[] args) {
-      System.out.println(solution(6, new int[] {7, 10}));
-    }
+    return customers[n - 1].time;
+  }
+
+  static class Customer {
+    long time = 0;
+    long num = 0;
+
+    public Customer(long time, long num) {
+      this.time = time;
+      this.num = num;
+    } 
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(6, new int[] {7, 10}));
+  }
 }
